a92ae4f6406a75b9e5f750f54e0a07bfa743af72
Mariam Nasser <mariamnasser979@gmail.com>
2021-04-06 20:33:05 -0700

    Added CategoryMeals after Filtering using where statement.

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:meal_app/dummy_data.dart';
 
 
 class CategoryMealsScreen extends StatelessWidget {
@@ -11,10 +12,21 @@ class CategoryMealsScreen extends StatelessWidget {
     final routeArg = ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryId = routeArg['id'];
     final categoryTitle = routeArg['title'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+
+      return meal.categories.contains(categoryId); // return meal after filtering..
+
+    }).toList();
 
     return Scaffold(
       appBar: AppBar(title:Text(categoryTitle)),
-      body: null,
+      body: ListView.builder(
+        itemBuilder: (ctx, index){
+          return Text(categoryMeals[index].title);
+
+
+        },
+        itemCount: categoryMeals.length ,),
     );
   }
 }
