da7c7c343d6ead25169356067c99d407d9782c02
Mariam Nasser <mariamnasser979@gmail.com>
2021-04-06 19:22:57 -0700

    Added CategoryScreen & CategoryItem.

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/cupertino.dart';
+import 'package:meal_app/dummy_data.dart';
+import 'package:meal_app/screens/widgets/category_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return GridView(
+      padding: EdgeInsets.all(25),
+      children: DUMMY_CATEGORIES.map((catDate) =>
       
+
+CategoryItem(catDate.id, catDate.title, catDate.color)
+      ).toList(),
+
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3/2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+        ),
     );
-  }
+  } 
 }
